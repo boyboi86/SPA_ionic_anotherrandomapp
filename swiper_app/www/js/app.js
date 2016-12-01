e16ff1855f07cde3fb859cbf9e0dc053a4724827
boyboi86 <boyboi86@gmail.com>
2016-12-01 20:48:01 +0800

    added swiper

--- swiper_app/www/js/app.js
+++ swiper_app/www/js/app.js
@@ -1,20 +1,24 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+(function(){
+var app = angular.module('starter', ['ionic'])
+
+app.controller('slideCtrl', function($scope){
+
+  $scope.slides = [];
+  
+  for(var i = 1; i <= 5; i++){
+    $scope.slides.push({
+      'title': 'Slides #' + i,
+      'description': 'index of ' + i
+    });
+  }
+
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
@@ -22,3 +26,4 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+}());
