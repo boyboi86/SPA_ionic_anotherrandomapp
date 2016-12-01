5ce15e8aaa44e95c95193282724892de8bba14e1
boyboi86 <boyboi86@gmail.com>
2016-12-01 19:54:23 +0800

    side menu added

--- www/js/app.js
+++ www/js/app.js
@@ -3,35 +3,51 @@
   var app = angular.module('starter', ['ionic'])
 
 app.config(function($stateProvider, $urlRouterProvider){
-  /*For state configuration has to have a views object
-    with the name of the ion-nav-view tag
-    then embed templateUrl within it*/
+
   $stateProvider.state('home', {
-    url:'/home',
-    views: {
-      'tab-home': {
-        templateUrl: 'templates/home.html'
-      }
-    }
-  })
-  /*now details view share the same state history as tab-home*/ 
+    url: '/home',
+    templateUrl: 'templates/home.html'
+  });
+
   $stateProvider.state('details', {
-    url:'/details',
-    views: {
-      'tab-home': {
-        templateUrl: 'templates/details.html'
-      }
-    }
-  })
+    url: '/details',
+    templateUrl: 'templates/details.html'
+  });
 
   $stateProvider.state('settings', {
-    url:'/settings',
-    views: {
-      'tab-settings': {
-        templateUrl: 'templates/settings.html'
-      }
-    }
-  })
+    url: '/settings',
+    templateUrl: 'templates/settings.html'
+  });
+
+  /* Tabs: For state configuration has to have a views object
+    with the name of the ion-nav-view tag
+    then embed templateUrl within it*/
+  // $stateProvider.state('home', {
+  //   url:'/home',
+  //   views: {
+  //     'tab-home': {
+  //       templateUrl: 'templates/home.html'
+  //     }
+  //   }
+  // })
+  /*now details view share the same state history as tab-home*/
+  // $stateProvider.state('details', {
+  //   url:'/details',
+  //   views: {
+  //     'tab-home': {
+  //       templateUrl: 'templates/details.html'
+  //     }
+  //   }
+  // })
+
+  // $stateProvider.state('settings', {
+  //   url:'/settings',
+  //   views: {
+  //     'tab-settings': {
+  //       templateUrl: 'templates/settings.html'
+  //     }
+  //   }
+  // })
 
   $urlRouterProvider.otherwise('/home');
 })
