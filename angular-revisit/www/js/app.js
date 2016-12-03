d3d52c1a62bd3ec08e482eb551e2a8640aab6f9f
boyboi86 <boyboi86@gmail.com>
2016-12-03 12:03:03 +0800

    angular-revisit $q

--- angular-revisit/www/js/app.js
+++ angular-revisit/www/js/app.js
@@ -1,11 +1,77 @@
-// Ionic Starter App
+(function(){
+var app = angular.module('starter', ['ionic'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
+app.controller('appCtrl', function($q, $scope, $timeout){
+
+  // function add(x, y){
+  //   return x + y;
+  // }
+  // $scope.result = add(5, 2);
+
+  // function add(x, y, callback){
+  //   $timeout(function(){
+  //     callback(x + y);
+  //   }, 1000)
+  // }
+  // var timenow = Date.now();
+  // add(5, 2, function(result){
+  //   add(result, 3, function(result){
+  //     $scope.result = result;
+  //     $scope.elapsed = Date.now() - timenow;
+  //   });
+  // });
+  var q = $q.defer();
+
+  function sum(x,y){
+    $timeout(function(){
+      var result = x + y;
+      if(result >= 0){
+        q.resolve(result);
+      } else {
+        q.reject('Negative result: ' + result);
+      }
+    }, 100)
+    return q.promise;
+  }
+
+  sum(1, -10)
+  .then(function(result){
+    return sum(result, 1)
+  })
+  .then(function(result){
+    $scope.newResult = result;
+  })
+  .catch(function(err){
+    $scope.err = err;
+  })
+  .finally(function(){
+    $scope.msg = 'no matter what happens this fn will run'
+  })
+
+  function add(x,y){
+    return $timeout(function(){
+      return x + y;
+    },200)
+  }
+
+  var timenow = Date.now();
+  var promise = add(5,2);
+  promise
+  .then(function(result){
+    return add(result, 3);
+  })
+  .then(function(result){
+    return add(result, 0);
+  })
+  .then(function(result){
+    $scope.result = result;
+    $scope.elapsed = Date.now() - timenow;
+  })
+
+})
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -22,3 +88,4 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+}())
